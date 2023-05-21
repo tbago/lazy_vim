bb144d341aaee858c06e2aa3285627c4eb9ec4cf
tbago <moonzalor@gmail.com>
2023-05-21 17:45:03 +0800

    First lazyvim version

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,45 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- buffers
+if Util.has("bufferline.nvim") then
+  map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+  map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+else
+  map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+  map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+end
+
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "<leader>wq", "<C-w>q") -- 关闭窗口
+
+vim.keymap.set("n", "<leader><leader>f", ":!clang-format -i % <CR>")
---
+++ lua/config/options.lua
@@ -0,0 +1,42 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+-- 禁用备份文件
+opt.backup = false
+opt.swapfile = false
+
+-- 行号
+opt.relativenumber = true
+opt.number = true
+
+-- 缩进
+opt.tabstop = 4
+opt.smartindent = true --识别{缩进，识别C语言关键字缩进
+opt.autoindent = true --自动缩进
+opt.shiftwidth = 4
+opt.expandtab = true --制表符替换为空格
+
+opt.fileencoding = "utf-8" -- 文件编码
+
+-- 防止折行
+opt.wrap = false
+
+-- 光标行高亮
+opt.cursorline = true
+
+-- 启用鼠标
+opt.mouse:append("a")
+
+-- 启用剪切板
+opt.clipboard:append("unnamedplus")
+
+-- 默认新窗口右和下
+opt.splitright = true
+opt.splitbelow = true
+
+-- 搜索
+opt.ignorecase = true
+opt.smartcase = true
